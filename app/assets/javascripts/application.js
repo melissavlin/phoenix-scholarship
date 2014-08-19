08a2446e7fcab13b126766ad4ee8a602288a1d1e
Melissa Lin <melissavlin@gmail.com>
2014-08-19 14:26:36 -0400

    made app review page more dynamic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,23 +15,39 @@
 //= require turbolinks
 //= require_tree .
 
-
-
-$(document).ready(function(e){
-	$(".app_num").on("click", function(e){
+$(document).on('ready page:load', function(){
+// $(document).ready(function(){
+	$(".link_app").on("click", function(e){
 		showHideApp(e);
-	});
+	})
+	var donation_amt = document.getElementById('donation_amt');
+	donation_amt.addEventListener('blur', triggerQ, false);
+	donation_amt.addEventListener('keyup', triggerQ, false);
 });
 
-var showHideApp = function(e){
-	// alert("works");
-	var selector = $(e.delegateTarget).closest('.review_app_wrapper').contents('.review_app').toggle();
-	$(".review_app").hide();
-	$(selector).show();
-	console.log("click");
-	// var selector = $(this).data("toggle");  // get corresponding element
- //  $("div").hide();
- //  $(selector).show();
+
+function triggerQ() {
+	var question = document.getElementById('tag_role');
+	(parseInt(this.value) >= 1 ? question.disabled = "" : question.disabled = 'disabled');
+	(parseInt(this.value) >= 1 ? question.checked = true : question.checked = false);
 }
+
+	var showHideApp = function(e){
+		console.log($(e.delegateTarget).prop("id"));
+
+		var link = ($(e.delegateTarget).prop("id")).substring(5);
+			console.log(link);
+
+		$('.app').addClass('hidden');
+		$("#"+link).removeClass('hidden');
+			// var selector = $(e.delegateTarget).closest('.review_app_wrapper').siblings().contents(link).toggle(); 
+			// $(".review_app").hide();
+			// $(selector).show();
+	}
+
+
+
+
+
 
 
