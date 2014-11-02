55dd56ad87d3e6b61a4e80f4667885b46dc87be6
Melissa Lin <melissavlin@gmail.com>
2014-11-01 21:48:51 -0400

    added after sign up notice closed #15

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,10 +5,20 @@ class RegistrationsController < Devise::RegistrationsController
 	  	# authorize! :manage, Donation
 	end
 
+
 protected
 
   def after_update_path_for(resource)
-  		root_path
+  		# root_path
+  		# user_path(resource)
+  		edit_user_registration_path
   end
 
+ def after_sign_up_path_for(resource)
+      new_user_registration_path
+  end
+
+	def after_inactive_sign_up_path_for(resource)
+	  new_user_registration_path
+	end
 end
